c7f9daebda6bdb5a1ac800ff73f8dc66f85ec1b8
Kellogg, Eric <ekellogg3@gmail.com>
2022-02-10 11:31:04 -0600

    started work on hw4 main

--- hw4.cc
+++ hw4.cc
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
-
-int main()
+int main(int argc, char *argv[])
 {
-
-
+	int fd;
+	fd = open(argv[1], O_RDWR); 
+	if(fd == -1)
+	{
+		perror("opening file");
+	}
+	if(!fd)
+	{
+		fd = open(argv[1], O_CREAT, 000);
+	}
 
 
 	return 0;
