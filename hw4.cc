1eb445a2a8c5439a1c562b52200011ba08c2d6ba
Eric Kellogg <z1942596@hopper.cs.niu.edu>
2022-02-11 20:57:22 -0600

    added documentation

--- hw4.cc
+++ hw4.cc
@@ -4,6 +4,22 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <cstring>
+
+/**
+ * Eric Kellogg
+ * z1942596
+ *
+ * CSCI 330 section 1
+ *
+ *
+ * I certify that this is my own work and where
+ * appropriate an extension of the starter code
+ * provided for the assignment
+ *
+ *
+ *
+ *
+ * *****************************************/
 int main(int argc, char *argv[])
 {
 	bool gotc = false;
@@ -13,9 +29,9 @@ int main(int argc, char *argv[])
 	char input2[] = "\n";
 	char *buffer2 = (char *) input2;
 	char *buffer;
-
-	while((opt = getopt(argc, argv, "c")) != -1)
-	{
+	
+	while((opt = getopt(argc, argv, "c")) != -1) ///checks to see if the optional argument "-c" was added
+	{					    /// and then sets the variable gotc to true	
 		switch(opt)
 		{
 			case 'c':
@@ -24,26 +40,26 @@ int main(int argc, char *argv[])
 		}
 	}	
 	
-	if((open(argv[optind],O_WRONLY)) == -1)
+	if((open(argv[optind],O_WRONLY)) == -1) /// checks if the file exists and if not it creates a new file
 	{
 		fd = open(argv[optind], O_WRONLY | O_CREAT, 000);
 		close(fd);	
 	}
-	if(stat(argv[optind], &filestatus) == -1)
+	if(stat(argv[optind], &filestatus) == -1) 
 	{
 		perror("Error in stat");
 		exit(1);
 	}
 
-	if((filestatus.st_mode) != 32768)
+	if((filestatus.st_mode) != 32768) /// makes sure the files permisions are set to 000
 	{
 		std::cout << "The file is not safe!" << std::endl;
 		exit(1);
 	}
 
 	buffer = argv[optind + 1];	
-	if(gotc == true)
-	{	
+	if(gotc == true) /// if the "-c" was added this will run and truncate the file before running anything
+	{					///takes in an argument from the command line and writes it to the file 
 		chmod(argv[optind], 500);
 		fd = open(argv[optind],O_WRONLY | O_TRUNC);
 		write(fd, buffer, strlen(buffer));
@@ -52,7 +68,7 @@ int main(int argc, char *argv[])
 		close(fd);
 			
 	}
-	else 
+	else  /// if no "-c" was present this runs and takes in an argument from the command line and writes it to the file
 	{
 		chmod(argv[optind], 500);
 		fd = open(argv[optind], O_APPEND | O_WRONLY);
