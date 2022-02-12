70d8ac7f3e793144426c40c83c40d7f7fea2a7eb
Kellogg, Eric <ekellogg3@gmail.com>
2022-02-11 20:40:52 -0600

    finished the project need to add documentation

--- hw4.cc
+++ hw4.cc
@@ -2,21 +2,64 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-
+#include <unistd.h>
+#include <cstring>
 int main(int argc, char *argv[])
 {
+	bool gotc = false;
+	int opt;
 	int fd;
-	fd = open(argv[1], O_RDWR); 
-	if(fd == -1)
+	struct stat filestatus;
+	char input2[] = "\n";
+	char *buffer2 = (char *) input2;
+	char *buffer;
+
+	while((opt = getopt(argc, argv, "c")) != -1)
 	{
-		perror("opening file");
+		switch(opt)
+		{
+			case 'c':
+			gotc = true;
+			break;
+		}
+	}	
+	
+	if((open(argv[optind],O_WRONLY)) == -1)
+	{
+		fd = open(argv[optind], O_WRONLY | O_CREAT, 000);
+		close(fd);	
 	}
-	if(!fd)
+	if(stat(argv[optind], &filestatus) == -1)
 	{
-		fd = open(argv[1], O_CREAT, 000);
+		perror("Error in stat");
+		exit(1);
 	}
 
+	if((filestatus.st_mode) != 32768)
+	{
+		std::cout << "The file is not safe!" << std::endl;
+		exit(1);
+	}
 
+	buffer = argv[optind + 1];	
+	if(gotc == true)
+	{	
+		chmod(argv[optind], 500);
+		fd = open(argv[optind],O_WRONLY | O_TRUNC);
+		write(fd, buffer, strlen(buffer));
+		write(fd, buffer2, strlen(buffer2));
+		chmod(argv[optind], 000);
+		close(fd);
+			
+	}
+	else 
+	{
+		chmod(argv[optind], 500);
+		fd = open(argv[optind], O_APPEND | O_WRONLY);
+		write(fd, buffer, strlen(buffer));
+		write(fd, buffer2, strlen(buffer2));
+		chmod(argv[optind], 000);
+		close(fd);
+	}
 	return 0;
 }
